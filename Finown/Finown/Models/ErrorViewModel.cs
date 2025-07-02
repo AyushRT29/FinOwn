2e28e189c826e000ab44f35ebc9cffe98753fe8f
Ayush <114756439+AyushRT29@users.noreply.github.com>
2025-07-02 22:15:02 +0530

    FinOwn

---
+++ Finown/Finown/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Finown.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Finown/Finown/Models/SpendSmartDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Finown.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }   
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
